45e569730f9393fe2cfc516ee397e0ef8f7c0baf
Pedrinhooo <50207991+Pedrinhooo@users.noreply.github.com>
2022-10-11 17:32:29 -0300

    Final(Js atualizado)

--- Script.js
+++ Script.js
@@ -1,68 +1,12 @@
-var cliqueA = window.document.getElementById("calcularA");
-cliqueA.addEventListener('click', clicarA);
+var qnt;
 
-var saiuA = window.document.getElementById("calcularA");
-saiuA.addEventListener('mouseout', sairA);
+function preco() {
 
-function sairA(){
-    saiuA.innerHTML = "Calcular"
+	var quantidade = document.getElementById("qtdtt").value;
+    var tipo = document.getElementById("tipott").value;
+	var valor = 40;
+	var total = valor * quantidade * tipo;
+	document.getElementById("total").innerHTML = total;
+
+	qnt = quantidade;
 }
-function clicarA(){
-    cliqueA.innerHTML = "clicou";
-
-
-
-    var restA = window.document.getElementById("resA");
-   
-    var valorQtdA = qtdA.options[qtdA.selectedIndex].value;
-    var totalA = valorQtdA * 90.00;
-  
-    restA.innerHTML = totalA.toFixed(2).replace('.',','); 
-
-
-}
-
-
-
-var cliqueB = document.getElementById("calcularB");
-cliqueB.addEventListener('click', clicarB);
-
-function clicarB(){
-    var restB = window.document.getElementById("resB");
-    var valorQtdB = qtdB.options[qtdB.selectedIndex].value;
-    var totalB = valorQtdB * 60.00;
-    restB.innerHTML = totalB.toFixed(2).replace('.',','); 
-}
-
-var saiuB = window.document.getElementById("calcularB");
-saiuB.addEventListener('mouseout', sairB);
-
-function sairB(){
-    saiuB.innerHTML = "Calcular"
-}
-
-
-
-
-var cliqueC = document.getElementById("calcularC");
-cliqueC.addEventListener('click', clicarC);
-
-function clicarC(){
-    var restC = window.document.getElementById("resC");
-    var valorQtdC = qtdC.options[qtdC.selectedIndex].text;
-    var totalC = valorQtdC * 30.00; 
-
-    restC.innerHTML = totalC.toFixed(2).replace('.',',');
-}
-var saiuC = window.document.getElementById("calcularC");
-saiuC.addEventListener('mouseout', sairC);
-
-function sairC(){
-    saiuC.innerHTML = "Calcular"
-}
-
-
-
-
-
-
